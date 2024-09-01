41be5aced5dee240da1aa7a1af4ec1375d403784
Şule <133784057+mahdildar32@users.noreply.github.com>
2024-09-01 15:09:13 +0300

    Update streamlit_app.py
    güncelleme

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,46 +2,63 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
 giris_page = st.Page(
-    page = "✋_app.py",
-    title = "Teknik Mülakata Giriş",
-    icon = ":material/account_circle:",
-    default = True,    
+    page="app.py",
+    title="Teknik Mülakata Giriş",
+    icon=":material/account_circle:",
+    default=True,    
 )
 
 llama_page = st.Page(
-    page = "🦙_llama.py",
-    title = "llama",
-    icon = ":material/account_circle:",
-    default = True,    
+    page="🦙_llama.py",
+    title="llama",
+    icon=":material/account_circle:",
+    default=True,    
 )
 
 mistral_page = st.Page(
-    page = "🤖_mistral.py",
-    title = "mistral",
-    icon = ":material/account_circle:",
-    default = True,    
+    page="🤖_mistral.py",
+    title="mistral",
+    icon=":material/account_circle:",
+    default=True,    
 )
 
-
 gpt_page = st.Page(
-    page = "⚛️_chatgpt.py",
-    title = "ChatGPT",
-    icon = ":material/account_circle:",
-    default = True,    
+    page="⚛️_chatgpt.py",
+    title="ChatGPT",
+    icon=":material/account_circle:",
+    default=True,    
 )
 
 mulakat_page = st.Page(
-    page = "🗣️_mulakat.py",
-    title = "Karşılıklı Mülakat",
-    icon = ":material/account_circle:",
-    default = True,    
+    page="🗣️_mulakat.py",
+    title="Karşılıklı Mülakat",
+    icon=":material/account_circle:",
+    default=True,    
 )
 
-
-
-pg = st.navigation(pages=[st.Page("✋_app.py"), st.Page("🦙_llama.py"), st.Page("🤖_mistral.py"), 
-                          st.Page("⚛️_chatgpt.py"), st.Page("🗣️_mulakat.py")])
+pg = st.navigation(pages=[
+    st.Page("app.py"), 
+    st.Page("🦙_llama.py"), 
+    st.Page("🤖_mistral.py"), 
+    st.Page("⚛️_chatgpt.py"), 
+    st.Page("🗣️_mulakat.py")
+])
 
 pg.run()
+
+with st.sidebar:
+    st.markdown(
+        """
+        <style>
+        .sidebar-text {
+            margin-top: 50px;
+            font-size: 14px;
+        }
+        </style>
+        <div class="sidebar-text">
+            Lütfen yukarıdan mülakat deneyimi oluşturmak istediğiniz büyük dil modelini seçin.
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
