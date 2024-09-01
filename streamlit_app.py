2eafcd0140d3bf77ffc7b2e28633b55ad3d28bc5
Ahmet Yusuf Kartal <a.yusufkartal@gmail.com>
2024-09-01 19:50:20 +0300

    Sayfa Kontrol Dosyası Hataları Düzeltildi

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,48 +2,47 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 giris_page = st.Page(
-    page="app.py",
-    title="Teknik Mülakata Giriş",
-    icon=":material/account_circle:",
-    default=True,    
+    page = "✋_app.py",
+    title = "Teknik Mülakata Giriş",
+    icon = ":material/account_circle:",
+    default = True,    
 )
 
 llama_page = st.Page(
-    page="🦙_llama.py",
-    title="llama",
-    icon=":material/account_circle:",
-    default=True,    
+    page = "🦙_llama.py",
+    title = "llama",
+    icon = ":material/account_circle:",
+    default = True,    
 )
 
 mistral_page = st.Page(
-    page="🤖_mistral.py",
-    title="mistral",
-    icon=":material/account_circle:",
-    default=True,    
+    page = "🤖_mistral.py",
+    title = "mistral",
+    icon = ":material/account_circle:",
+    default = True,    
 )
 
+
 gpt_page = st.Page(
-    page="⚛️_chatgpt.py",
-    title="ChatGPT",
-    icon=":material/account_circle:",
-    default=True,    
+    page = "⚛️_chatgpt.py",
+    title = "ChatGPT",
+    icon = ":material/account_circle:",
+    default = True,    
 )
 
 mulakat_page = st.Page(
-    page="🗣️_mulakat.py",
-    title="Karşılıklı Mülakat",
-    icon=":material/account_circle:",
-    default=True,    
+    page = "🗣️_mulakat.py",
+    title = "Karşılıklı Mülakat",
+    icon = ":material/account_circle:",
+    default = True,    
 )
 
-pg = st.navigation(pages=[
-    st.Page("app.py"), 
-    st.Page("🦙_llama.py"), 
-    st.Page("🤖_mistral.py"), 
-    st.Page("⚛️_chatgpt.py"), 
-    st.Page("🗣️_mulakat.py")
-])
+
+
+pg = st.navigation(pages=[st.Page("✋_app.py"), st.Page("🦙_llama.py"), st.Page("🤖_mistral.py"), 
+                          st.Page("⚛️_chatgpt.py"), st.Page("🗣️_mulakat.py")])
 
 pg.run()
 
